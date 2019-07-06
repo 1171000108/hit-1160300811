e63257fce08de32509356391dd30aabcf01879c0
1171000108 <zwx19990619@163.com>
2019-07-07 00:11:05 +0800

    add some codes

--- src/HelloWorld.c
+++ src/HelloWorld.c
@@ -4,6 +4,9 @@ int main(int argc, char const *argv[]) {
 	/*
 	   feel free to change the code
 	 */
-	printf("%s\n", "Hello World!");
+    printf("Please input your name:\n");
+    char name[100];
+    scanf("%s",name);
+	printf("%sI'm %s!\n", "Hello World!",name);
 	return 0;
 }

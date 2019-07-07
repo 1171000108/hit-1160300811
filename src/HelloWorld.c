8c647aaf132a1cca41c76f6557446831ad31b5f2
1171000108 <zwx19990619@163.com>
2019-07-07 18:13:16 +0800

    make codes safer

--- src/HelloWorld.c
+++ src/HelloWorld.c
@@ -7,6 +7,7 @@ int main(int argc, char const *argv[]) {
     printf("Please input your name:\n");
     char name[100];
     scanf("%s",name);
+    sscanf(name,"%[a-zA-Z]",name);
 	printf("%sI'm %s!\n", "Hello World!",name);
 	return 0;
 }
